43174c7f48cf6ff05c5497ccb8508c3ee57a17f5
Cnjerii <christinenjeri162@gmail.com>
2022-04-27 09:32:14 +0300

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ */
+int main (void)
+{
+	wwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+
+}

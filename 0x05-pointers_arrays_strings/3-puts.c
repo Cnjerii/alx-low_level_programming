fee86cb2d437f3550781ac12cb93af7e815eb091
Cnjerii <christinenjeri162@gmail.com>
2022-06-14 00:42:40 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _putchar - prints string
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: void means is correct
+ */
+
+void _putchar('char*str')
+{
+	char *c;
+	int m;
+
+	c = str;
+
+		for (m = 0; c[m]; m++)
+		{
+			_putchar (c[m]);
+		}
+	_putchar ('\n');
+}
+

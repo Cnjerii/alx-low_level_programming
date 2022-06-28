66c2131c49816bf190ed60d878a7b2ef35fdb92d
Cnjerii <christinenjeri162@gmail.com>
2022-06-28 02:30:11 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,43 +7,33 @@
  */
 void times_table(void)
 {
-	int a;
-	int b;
-	int c;
+	int x, y;
 
-	for (a = 0; a <= 9; a++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (y = 0; y <= 9; y++)
 		{
-			c = a * b;
-			if ((c / 10) == 0)
+			int multi = x * y;
+
+			if (y == 0)
 			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar ('0');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
+				_putchar('0');
+			}
+			else if (multi <= 9)
 				{
 					_putchar (',');
 					_putchar (' ');
+					_putchar (' ');
+					_putchar (multi + '0');
 				}
-			}
 			else
-			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
 				{
 					_putchar (',');
 					_putchar (' ');
+					_putchar (multi / 10 + '0');
+					_putchar (multi % 10 + '0');
 				}
 			}
-		}
 		_putchar ('\n');
 	}
 }

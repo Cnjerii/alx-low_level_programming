5add0e48a8b8ac07968015a17c530b77b617148d
Cnjerii <christinenjeri162@gmail.com>
2022-07-02 02:05:59 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define UNUSED(X) (void)(x)
+
+/**
+ * main - main function
+ * @argc: arguments
+ * @argv: Array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	UNUSED(argc);
+	printf("%\n", argv[0]);
+	return (0);
+}

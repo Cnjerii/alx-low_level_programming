8a8d0bd73d5bf4f07c066a052a01fc7ade81eb60
Cnjerii <christinenjeri162@gmail.com>
2022-08-03 05:10:32 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,19 +19,19 @@ int _strlen(char *s)
 }
 
 /**
- * create_file -creates a file 
+ * create_file -creates a file
  * @filename: name of file to create
  * @text_content: text to write
  *
  * Return: 1 on success0 on failure
  */
-intt create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes = 0, len = _strlen9text_content;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
-		return(-1);
+		return (-1);
 	fd = open(filename, O_WRONLY | 0_CREAT | 0_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,6 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
+
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
